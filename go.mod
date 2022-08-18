27880c135319d629f9cd3dfb0a62594610764082
Fabricio Xavier <fabricio.xavier@luizalabs.com>
2022-08-17 23:55:17 -0300

    Update

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/arsmn/fiber-swagger/v2 v2.31.1
+	github.com/bytedance/sonic v1.3.5
 	github.com/gofiber/fiber/v2 v2.36.0
 	github.com/google/uuid v1.3.0
 	github.com/onrik/gorm-logrus v0.4.0
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/bytedance/sonic"
 	router "github.com/fsvxavier/unico/api"
 	"github.com/fsvxavier/unico/database"
 	_ "github.com/fsvxavier/unico/docs"
@@ -45,7 +46,11 @@ func main() {
 	currentTime := time.Now()
 
 	// Start a new fiber app
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		Prefork:     false,
+		JSONEncoder: sonic.Marshal,
+		JSONDecoder: sonic.Unmarshal,
+	})
 
 	app.Use(recover.New())
 
